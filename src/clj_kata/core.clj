bf2830877bdfb232c6c1bcbd3741e8e20438df80
priska <priska@23bit.net>
2013-02-13 10:32:19 +0100

    added a quicksort implementation.

--- src/clj_kata/core.clj
+++ src/clj_kata/core.clj
@@ -4,3 +4,11 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn qsort [L]
+  (if (empty? L) 
+      '()
+      (let [[pivot & L2] L]
+           (lazy-cat (qsort (for [y L2 :when (<  y pivot)] y))
+                     (list pivot)
+                     (qsort (for [y L2 :when (>= y pivot)] y))))))
