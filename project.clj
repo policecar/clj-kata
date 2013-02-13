1e2cde6172a5e218f329453622a4d9255f1c8ccc
priska <priska@23bit.net>
2013-02-13 10:46:02 +0100

    a bit of project description.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-kata "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Little, collected snippets to learn some Clojure."
+  :url "https://github.com/policecar/clj-kata"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
